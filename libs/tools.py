c763c5dccb95236abfe6f7ac783121aad4b9e80e
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2022-11-14 15:20:13 +0400

    Added new tools features

--- libs/tools.py
+++ libs/tools.py
@@ -2,30 +2,113 @@ from genericpath import isfile
 import json
 from ntpath import join
 from os import listdir, remove
-
+from geojson_rewind import rewind
 
 visas_location = "./visas/"
 
+countries_location = "./countries/countries.json"
+data_location = "./countries/data/"
 
-def load_files():
-    visa_countries = {}
+def load_visas():
+    visa_countries = {
+        "csvcid": {},
+        "cca2": {},
+        "cca3": {}
+    }
     visa_files = [f for f in listdir(visas_location) if isfile(join(visas_location, f))]
     for filename in visa_files:
         if filename.endswith(".visas.json"):
             with open(visas_location + filename, "r") as r:
                 visa_country_inst = json.loads(r.read())
-                visa_countries[visa_country_inst["csvcid"]] = visa_country_inst
-    
+                visa_countries["csvcid"][visa_country_inst["csvcid"]] = visa_country_inst
+
+                if visa_country_inst["cca2"] != "":
+                    visa_countries["cca2"][visa_country_inst["cca2"]] = visa_country_inst
+
+                if visa_country_inst["cca3"] != "":
+                    visa_countries["cca3"][visa_country_inst["cca3"]] = visa_country_inst
+
     return visa_countries
 
-def save_files(visa_countries):
-    for csvc in visa_countries:
-        with open(visas_location + csvc + ".visas.json", "w") as w:
-            w.write(json.dumps(visa_countries[csvc], sort_keys=True, indent=4, separators=(',', ': ')))
+def save_visas(visa_countries):
+    for csvcid in visa_countries["csvcid"]:
+        with open(visas_location + csvcid + ".visas.json", "w") as w:
+            w.write(json.dumps(visa_countries["csvcid"][csvcid], sort_keys=True, indent=4, separators=(',', ': ')))
 
-def remove_csvcid(csvcid):
+def remove_csvc_csvcid(csvcid):
     remove(visas_location + csvcid + ".visas.json")
-        
+
+def load_geo():
+    geo_countries = {
+        "csvcid": {},
+        "cca2": {},
+        "cca3": {}
+    }
+
+    geo_files = [f for f in listdir(data_location) if isfile(join(data_location, f)) and f.endswith("geo.json")]
+    for filename in geo_files:
+        try:
+            with open(data_location + filename, "r") as r:
+                geo_inst = json.loads(r.read())
+
+                feature = geo_inst["features"][0]
+
+                cca3 = filename.replace(".geo.json", "").upper()
+                
+                geo_countries["cca2"][feature["properties"]["cca2"].upper()] = feature
+                geo_countries["cca3"][cca3.upper()] = feature
+
+                # if "cca3" in feature["properties"] and feature["properties"]["cca3"] != "":
+                #     geo_countries["cca3"][feature["properties"]["cca3"]] = feature
+
+                if "csvcid" in feature["properties"] and feature["properties"]["csvcid"] != "":
+                    geo_countries["csvcid"][feature["properties"]["csvcid"]] = feature
+
+        except Exception as e:
+            print(filename)
+
+    return geo_countries
+
+def save_geo(geo_features_list: dict[str, dict[str, any]]):
+    for key in geo_features_list["cca3"]:
+        print(key)
+        with open(data_location + key.lower() + ".geo.json", "w") as w:
+            w.write(json.dumps({"type":"FeatureCollection","features":[geo_features_list["cca3"][key]]}))
+
+def save_visas(visa_countries):
+    for csvcid in visa_countries["csvcid"]:
+        with open(visas_location + csvcid + ".visas.json", "w") as w:
+            w.write(json.dumps(visa_countries["csvcid"][csvcid], sort_keys=True, indent=4, separators=(',', ': ')))
+
+def remove_csvc_csvcid(csvcid):
+    remove(visas_location + csvcid + ".visas.json")
+
+def load_countries():
+    countries_dict = {
+        "csvcid": {},
+        "cca2": {},
+        "cca3": {}
+    }
+
+    with open(countries_location, "r") as r:
+        countries_list = json.loads(r.read())
+
+        for country in countries_list:
+            if "csvcid" not in country:
+                print("NOT IN " + country["cca3"])
+            countries_dict["cca3"][country["cca3"]] = country
+            countries_dict["cca2"][country["cca2"]] = country
+            countries_dict["csvcid"][country["csvcid"]] = country
+
+    return countries_dict
+
+def save_countries(new_countries):
+    countries_list = []
+
+    for key in new_countries["cca3"]:
+        countries_list.append(new_countries["cca3"][key])
+    with open(countries_location, "w") as w:
+        w.write(json.dumps(countries_list, sort_keys=True, indent=4, separators=(',', ': ')))
 
 class tools:
     def __init__(self):
@@ -34,27 +117,25 @@ class tools:
     class countries:
         visa_countries = {}
         def __init__(self, args):
-            self.visa_countries = load_files()
+            self.visa_countries = load_visas()
 
-            print(args)
-
-            if args.subcommand == "add":
+            if args.countries_subcommand == "add":
                 self.add(args.csvc, args.name, args.cca2, args.cca3, args.ccn3)
 
-            if args.subcommand == "remove":
+            if args.countries_subcommand == "remove":
                 self.remove(args.csvc)
 
-
-            if args.subcommand == "contains":
+            if args.countries_subcommand == "contain":
                 if not self.contains(args.csvc, args.cca2, args.cca3):
                     print("Visas country does not exists!")
                 else:
                     print("Visas country exists!")
 
+
         def add(self, csvcid, name="", cca2="", cca3="", ccn3=""):
-            if csvcid in self.visa_countries:
+            if csvcid in self.visa_countries["csvcid"]:
                 raise Exception(f"Can not add {csvcid}. It already exists")
-            
+
             new_country = {
                 "cca2": cca2,
                 "cca3": cca3,
@@ -76,40 +157,134 @@ class tools:
                 "stay_length": {}
             }
 
-            for key in self.visa_countries:
+            for key in self.visa_countries["csvcid"]:
                 new_country["requirements"][key] = new_visa_req
-                self.visa_countries[key]["requirements"][csvcid] = new_visa_req
+                self.visa_countries["csvcid"][key]["requirements"][csvcid] = new_visa_req
 
-            self.visa_countries[csvcid] = new_country
+            self.visa_countries["csvcid"][csvcid] = new_country
 
-            save_files(self.visa_countries)
+            save_visas(self.visa_countries)
+
 
         def remove(self, csvcid, force = False):
-            if csvcid not in self.visa_countries and not force:
+            if csvcid not in self.visa_countries["csvcid"] and not force:
                 raise Exception(f"Can not delete {csvcid}. Does not exists!")       
 
-            for key in self.visa_countries:
-                if csvcid in self.visa_countries[key]["requirements"]:
-                    del self.visa_countries[key]["requirements"][csvcid]
+            for key in self.visa_countries["csvcid"]:
+                if csvcid in self.visa_countries["csvcid"][key]["requirements"]:
+                    del self.visa_countries["csvcid"][key]["requirements"][csvcid]
 
-            del self.visa_countries[csvcid]
+            del self.visa_countries["csvcid"][csvcid]
 
-            remove_csvcid(csvcid)
-            save_files(self.visa_countries)
-            
+            remove_csvc_csvcid(csvcid)
+            save_visas(self.visa_countries)
+
+
         def contains(self, csvcid="", cca2="", cca3=""):
-            if csvcid != "":
-                return self.visa_countries[csvcid] != None
+            if csvcid:
+                return csvcid in self.visa_countries["csvcid"]
 
-            if cca2 != "" or cca3 != "":
-                for key in self.visa_countries:
-                    if cca2 != "" and self.visa_countries[key]["cca2"] is cca2:
-                        return True
+            if cca2:
+                return cca2 in self.visa_countries["cca2"]
 
-                    if cca3 != "" and self.visa_countries[key]["cca3"] is cca3:
-                        return True
+            if cca3:
+                return cca3 in self.visa_countries["cca3"]
 
             return False
-    class visa:
+
+
+    class reform:
+        visa_countries = {}
+        countries_json = {}
+        geo_json = {}
+
+        def __init__(self, args):
+            self.visa_countries = load_visas()
+            self.countries_json = load_countries()
+            self.geo_json = load_geo()
+
+            if args.subcommand == "diff":
+                self.diff()
+
+            if args.subcommand == "refcountries":
+                self.inject_ccas_visas()
+
+            if args.subcommand == "csvcid":
+                self.inject_csvc()
+
+
+        def diff(self):
+            for csvc in self.countries_json["csvcid"]:
+                if csvc not in self.visa_countries["csvcid"]:
+                    print("Visas missing " + csvc)
+
+            for csvc in self.visa_countries["csvcid"]:
+                if csvc not in self.countries_json["csvcid"]:
+                    print("Countries missing " + csvc)
+
+            for cca2 in self.countries_json["cca2"]:
+                if cca2 not in self.geo_json["cca2"]:
+                    print("Geo missing " + cca2)
+            
+            # for key in self.countries_json["cca2"]:
+            #     if key not in self.visa_countries["cca2"]:
+            #         print("Countries: Visas missing CCA2 " + key)
+
+            # for key in self.countries_json["cca2"]:
+            #     if key not in self.geo_json["cca2"]:
+            #         print("Countries: Geo missing CCA2 " + key)
+                    
+        # def rewind_geo(self):
+        #     for cca3 in self.geo_json["cca3"]:
+        #         try:
+        #             input = json.dumps({"type":"FeatureCollection","features":[self.geo_json["cca3"][cca3]]})
+
+        #             rewind_out = rewind(input)
+        #             rewind_decoded = json.loads(rewind_out)
+        #             rewinded_feature = rewind_decoded["features"][0]
+
+        #             self.geo_json["cca3"][cca3] = rewinded_feature
+        #         except Exception as e:
+        #             print(cca3, e)
+
+        #     save_geo(self.geo_json)
+                
+        def inject_csvc(self):
+            for cca3 in self.geo_json["cca3"]:
+                del self.geo_json["cca3"][cca3]["cca2"]
+                del self.geo_json["cca3"][cca3]["cca3"]
+                del self.geo_json["cca3"][cca3]["ccn3"]
+                del self.geo_json["cca3"][cca3]["csvcid"]
+                del self.geo_json["cca3"][cca3]["name"]
+
+                self.geo_json["cca3"][cca3]["properties"]["cca2"] = self.countries_json["cca3"][cca3]["cca2"]
+                self.geo_json["cca3"][cca3]["properties"]["cca3"] = self.countries_json["cca3"][cca3]["cca3"]
+                self.geo_json["cca3"][cca3]["properties"]["ccn3"] = self.countries_json["cca3"][cca3]["ccn3"]
+                self.geo_json["cca3"][cca3]["properties"]["csvcid"] = self.countries_json["cca3"][cca3]["csvcid"]
+                self.geo_json["cca3"][cca3]["properties"]["name"] = self.countries_json["cca3"][cca3]["name"]["common"]
+
+            save_geo(self.geo_json)
+
+
+        def inject_csvc_countries(self):
+            for key in self.visa_countries["csvcid"]:
+                if self.visa_countries["csvcid"][key]["cca2"] != "" and self.visa_countries["csvcid"][key]["cca2"] in self.countries_json["cca2"]:
+                    self.countries_json["cca2"][self.visa_countries["csvcid"][key]["cca2"]]["csvcid"] = key
+
+
+            save_countries(self.countries_json)
+
+
+        def inject_ccas_visas(self):
+            for key in self.countries_json["csvcid"]:
+                self.visa_countries["csvcid"][key]["cca2"] = self.countries_json["csvcid"][key]["cca2"]
+                self.visa_countries["csvcid"][key]["cca3"] = self.countries_json["csvcid"][key]["cca3"]
+                self.visa_countries["csvcid"][key]["ccn3"] = self.countries_json["csvcid"][key]["ccn3"]
+                self.visa_countries["csvcid"][key]["name"] = self.countries_json["csvcid"][key]["name"]["common"]
+
+            save_visas(self.visa_countries)
+
+
+    class visas:
         def __init__(self, args):
             pass
--- tools.py
+++ tools.py
@@ -1,84 +1,56 @@
 #!/usr/bin/python3
 
-import os
 import argparse
-
-# SIZES = {
-#     "s": "Small",
-#     "m": "Medium",
-#     "l": "Large",
-#     "xl": "Extra large",
-#     "xxl": "Extra extra large"
-# }
-
-# CRUSTS = {"normal": "", "thin": " thin crust", "deep": " deep dish"}
-
-
-# def build_pizza(order):
-#     pizza = f"{SIZES[order.size]}{CRUSTS[order.crust]}"
-#     if order.toppings:
-#         pizza +=  " with " + ", ".join(order.toppings)
-#     if order.cheese:
-#         pizza += " plus extra cheese"
-#     if order.sauce:
-#         pizza += " and extra sauce"
-#     return pizza
-
+from libs.tools import tools
 
 """tool
 Usage:
-  tool.py build [ --geo | --info | --visa | --validate ]
-  tool.py visa add <cca2> <name> [--default-policy=<visa_type>] [--default-requirement=<visa_type>] [--force]
-  tool.py visa set <visa_type> <from-cca2> (--cross | --requirement | --policy) <to-cca2>... [--note=<note>] [--time=<time>]
-  tool.py visa rm <cca2> [--force]
-  
+    tool.py build [ --geo | --info | --visa | --validate ]
+    tool.py visa add <cca2> <name> [--default-policy=<visa_type>] [--default-requirement=<visa_type>] [--force]
+    tool.py visa set <visa_type> <from-cca2> (--cross | --requirement | --policy) <to-cca2>... [--note=<note>] [--time=<time>]
+    tool.py visa rm <cca2> [--force]
+    
 Options:
-  -h --help     Show this screen.
-  --version     Show version.
-  build
-  --geo         Validate and build geo  data
-  --info        Validate and build info data
-  --visa        Validate and build visa data
-  --validate    Validate all data
-  visa
-  -defp --default-policy=<type>      Sets default visa policy         [default: r] r - required
-  -defr --default-requirement=<type> Sets default visa requirement    [default: r] r - required
-  -f --force                      Force
-  -x --cross                      Cross visa set between this countries
-  -r --requirement                Sets requirement for the countrie to the countrie(s)
-  -p --policy                     Sets policy of the countrie to the countrie(s)
-  --note=<note>                   Sets note        [default: None]
-  --time=<time>                   Sets visa length [default: None]
+    -h --help     Show this screen.
+    --version     Show version.
+
+    build
+    --geo         Validate and build geo  data
+    --info        Validate and build info data
+    --visa        Validate and build visa data
+    --validate    Validate all data
+
+    visa
+    -defp --default-policy=<type>      Sets default visa policy         [default: r] r - required
+    -defr --default-requirement=<type> Sets default visa requirement    [default: r] r - required
+
+    -f --force                      Force
+    -x --cross                      Cross visa set between this countries
+    -r --requirement                Sets requirement for the countrie to the countrie(s)
+    -p --policy                     Sets policy of the countrie to the countrie(s)
+    --note=<note>                   Sets note        [default: None]
+    --time=<time>                   Sets visa length [default: None]
 """
-from libs.tools import tools
+
+def upperCaseCodes(args):
+    if "csvc" in vars(args) and args.csvc is not None:
+        args.csvc = args.csvc.upper()
+
+    if "cca3" in vars(args) and args.cca3 is not None:
+        args.cca3 = args.cca3.upper()
+    
+    if "cca2" in vars(args) and args.cca2 is not None:
+        args.cca2 = args.cca2.upper()
+
+    if "ccn3" in vars(args) and args.ccn3 is not None:
+        args.ccn3 = args.ccn3.upper()
+
+
+    return args
 
 def main():
     parser = argparse.ArgumentParser(prog="Visas Editing Tools", add_help=True)
-
     subparsers = parser.add_subparsers(dest = "command")
-
-    # ----- Countries CLI ----- #
-    countries_cmd = subparsers.add_parser("countries", help="Manage countries")
-    countries_subparsers = countries_cmd.add_subparsers(dest = "subcommand")
-
-    countries_list_subparsers = countries_subparsers.add_parser("list", help="List available countries")
-
-    countries_add_subparsers = countries_subparsers.add_parser("add", help="Add new country")
-    countries_add_subparsers.add_argument("--csvc", type=str, help="Visas2 country code", required=True)
-    countries_add_subparsers.add_argument("--name", type=str, help="Country name")
-    countries_add_subparsers.add_argument("--cca3", type=str, help="Alpha3 country code")
-    countries_add_subparsers.add_argument("--cca2", type=str, help="Alpha2 country code")
-    countries_add_subparsers.add_argument("--ccn3", type=str, help="Numeric3 country code")
-
-
-    countries_contains_subparsers = countries_subparsers.add_parser("contains", help="Find existing country")
-    countries_contains_subparsers.add_argument("--csvc", type=str, help="Visas2 country code")
-    countries_contains_subparsers.add_argument("--cca3", type=str, help="Alpha3 country code")
-    countries_contains_subparsers.add_argument("--cca2", type=str, help="Alpha2 country code")
-    countries_contains_subparsers.add_argument("--ccn3", type=str, help="Numeric3 country code")
-    
-    countries_remove_subparsers = countries_subparsers.add_parser("remove", help="Remove country")
-    countries_remove_subparsers.add_argument("--csvc", type=str, help="Visas2 country code", required=True)
 
     # ----- Visa relationship CLI ----- #
     visas_cmd = subparsers.add_parser("visas", help="Manage visa requirements")
@@ -90,19 +62,56 @@ def main():
     visas_set_subparsers.add_argument("--to-csvc", type=str, nargs="+", help="Countries affected", required=True)
     visas_set_subparsers.add_argument("--passport-pages", type=int, help="Min passport pages")
 
-    args = parser.parse_args()
+
+    # ----- Visa Countries CLI ----- #
+    visa_countries_cmd = visas_subparsers.add_parser("countries", help="Manage countries")
+    visa_countries_subparsers = visa_countries_cmd.add_subparsers(dest = "countries_subcommand")
+
+    countries_list_subparsers = visa_countries_subparsers.add_parser("list", help="List available countries")
+
+    visa_countries_add_subparsers = visa_countries_subparsers.add_parser("add", help="Add new country")
+    visa_countries_add_subparsers.add_argument("--csvc", type=str, help="Visas2 country code", required=True)
+    visa_countries_add_subparsers.add_argument("--name", type=str, help="Country name")
+    visa_countries_add_subparsers.add_argument("--cca3", type=str, help="Alpha3 country code")
+    visa_countries_add_subparsers.add_argument("--cca2", type=str, help="Alpha2 country code")
+    visa_countries_add_subparsers.add_argument("--ccn3", type=str, help="Numeric3 country code")
+
+
+    visa_countries_contains_subparsers = visa_countries_subparsers.add_parser("contain", help="Find existing country")
+    visa_countries_contains_subparsers.add_argument("--csvc", type=str, help="Visas2 country code")
+    visa_countries_contains_subparsers.add_argument("--cca3", type=str, help="Alpha3 country code")
+    visa_countries_contains_subparsers.add_argument("--cca2", type=str, help="Alpha2 country code")
+    visa_countries_contains_subparsers.add_argument("--ccn3", type=str, help="Numeric3 country code")
+    
+    visa_countries_remove_subparsers = visa_countries_subparsers.add_parser("remove", help="Remove country")
+    visa_countries_remove_subparsers.add_argument("--csvc", type=str, help="Visas2 country code", required=True)
+
+
+    # ----- Visa relationship CLI ----- #
+    reforms_cmd = subparsers.add_parser("reform", help="Reform visas")
+    reforms_subparsers = reforms_cmd.add_subparsers(dest = "subcommand")
+
+    reforms_subparsers.add_parser("diff", help="Diff")
+    reforms_subparsers.add_parser("refcountries", help="Reform countries")
+    reforms_subparsers.add_parser("csvcid", help="Rewind geojson")
     # ---------- #
 
-    # print(args)
-    try:
-        if args.command == "countries":
+    args = parser.parse_args()
+    print(args)
+
+    args = upperCaseCodes(args)
+
+    # try:
+    if args.command == "visas":
+        if args.subcommand == "countries":
             tools.countries(args)
-            
-        if args.command == "visas":
+        else:
             tools.visas(args)
-    except Exception as e:
-        print("Error")
-        print(e)
+    elif args.command == "reform":
+        tools.reform(args)
+        
+    # except Exception as e:
+    #     print("Error", e)
 
 if __name__ == "__main__":
     main()
